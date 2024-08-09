ffd0a585f71f6ccab28588245ae3fbc6bdbaadeb
getenjr <32555548+getenjr@users.noreply.github.com>
2024-08-10 05:30:15 +0700

    Update lienol-diy-part2.sh

--- lienol-diy-part2.sh
+++ lienol-diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 #移除不用软件包    
 rm -rf feeds/packages/libs/libgd-full
